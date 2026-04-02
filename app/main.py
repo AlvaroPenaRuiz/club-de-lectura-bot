07b58bfceafbd5623a7f107dec7c0ff39e9026e0
Álvaro Pena Ruiz <alvaropenaruiz@gmail.com>
2026-04-02 23:55:42 +0200

    cambiando mensaje

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ logging.basicConfig(
 )
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Este es el Club de Lectura Bot. ¡Bienvenido!")
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(TOKEN).build()
